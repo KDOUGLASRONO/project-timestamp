f3a0428243fd671515237d548ed6354cf0de29a6
unknown <kdouglasrono@gmail.com>
2022-11-05 15:40:54 +0300

    yes

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/api/2015-1-25",(req,res)=>{
 
 //
 app.get("/api/1451001600000",(req,res)=>{
-  res.json({unix:Date.now,utc:Date()});
+  res.json({unix:Date.now(),utc:Date()});
 });
 
 // listen for requests :)

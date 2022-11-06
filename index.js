fe849c216278cba0f4b0116eb5287133b5037508
unknown <kdouglasrono@gmail.com>
2022-11-06 14:48:22 +0300

    more dates

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/api/:date_string",(req,res)=>{
   console.log(dateString)
   if(dateString.split("-").length>1){
     res.json({unix:Date.parse(dateString)});
-    res.json({utc:Date(Number(dateString)).toUTCString()});
+    res.json({utc:Date(Number(dateString))});
   }
   else if(!isNaN(dateString)){
     console.log(dateString);
